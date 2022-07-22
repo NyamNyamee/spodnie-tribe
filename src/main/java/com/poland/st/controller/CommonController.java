5ec61d4941632d511765ef8a9128c009f70d2197
inwoo <jopelee2@gmail.com>
2022-07-22 09:35:57 +0900

    shop product 메뉴 분리, 헤더 제목 스타일 수정

--- src/main/java/com/poland/st/controller/CommonController.java
+++ src/main/java/com/poland/st/controller/CommonController.java
@@ -3,22 +3,30 @@ package com.poland.st.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class CommonController {
+    @GetMapping("")
+    public String getMain(Model model) {
+        return "view/page/main";
+    }
 
     @GetMapping("/about")
     public String getAbout() {
-
         return "view/page/about";
     }
 
     @GetMapping("/contact")
     public String getContact() {
-
         return "view/page/contact";
+    }
+
+    @GetMapping("/shop")
+    public String getShop(Model model) {
+        return "view/page/shop";
     }
 }
